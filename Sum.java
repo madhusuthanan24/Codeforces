8e4ddd230841877690d5628b89abaf199e0e31df
madhusuthanan24 <madhusuthananp24cs@srishakthi.ac.in>
2026-09-25 21:43:06 +0530

    day 4

---
+++ Sum.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Sum{
+    public static void main(String args[]){
+        Scanner sc=new Scanner (System.in);
+        int n=sc.nextInt();
+        while(n-->0){
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c=sc.nextInt();
+            if(a+b==c){
+                System.out.println("YES");
+            }
+            else if(a+c==b){
+                System.out.println("YES");
+            }
+            else if(b+c==a){
+                System.out.println("YES");
+            }
+            else{
+                System.out.println("NO");
+            }
+        }
+        
+    }
+}
